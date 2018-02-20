19d51ea3a3bdfc4e630d04a370375353f81bf205
Niels Bergervoet <nielsbergervoet@gmail.com>
2018-02-20 16:27:24 +0100

    added encryption to the sso cookie

--- sso_idp/src/index.php
+++ sso_idp/src/index.php
@@ -12,7 +12,11 @@
         if($authSucceeded) {
             $session = $authenticator->getSession();
             $sys_name = $systemService->getSystemNameById($session->system_id);
-            $SSOCookie = 'SSO_id=' . $session->system_id . '.' . $session->user_id . '';
+            $SSOCookie = 'SSO_id='
+                . base64_encode($session->system_id)
+                . '.' . base64_encode($session->user_id)
+                . '.' . hash('sha256', SSO_PASSWORD)
+                . ';HttpOnly';
             $sessionCookie = 'session_id_' . $sys_name . '=' . $session->session_id . ';HttpOnly' .
                 ';Expires=' . date('D, d M Y H:i:s', $session->expires_at_timestamp);
             header('Set-Cookie: ' . $sessionCookie, false);
@@ -24,16 +28,26 @@
             echo 'invalid credentials given';
         }
     }
-    else if(isset($_COOKIE['SSO_id'])) {
+    else if(isset($_COOKIE['SSO_id']) && isset($_GET['origin'])) {
         $values = preg_split('/[.]/', $_COOKIE['SSO_id']);
-        $session = $sessionManager->getSessionForUser($values[0], $values[1]);
-        $sys_name = $systemService->getSystemNameById($systemService->getSystemByDomain($_GET['origin']));
-        $expires_at_timestamp = DateTime::createFromFormat('Y-m-d H:i:s', $session['expires_at'])->getTimestamp();
-        $sessionCookie = 'session_id_' . $sys_name . '=' . $session['session_id'] . ';HttpOnly' .
-            ';Expires=' . date('D, d M Y H:i:s', $expires_at_timestamp);
-        header('Set-Cookie: ' . $sessionCookie, false);
-        header('Location: ' . 'http://' . $_GET['origin']);
-        die();
+        if(hash('sha256', SSO_PASSWORD) == $values[2]) {
+            $session = $sessionManager->getSessionForUser(base64_decode($values[0]), base64_decode($values[1]));
+            $sys_name = $systemService->getSystemNameById($systemService->getSystemByDomain($_GET['origin']));
+            if(!empty($sys_name) && !empty($session)) {
+                $expires_at_timestamp = DateTime::createFromFormat('Y-m-d H:i:s', $session['expires_at'])->getTimestamp();
+                $sessionCookie = 'session_id_' . $sys_name . '=' . $session['session_id'] . ';HttpOnly' .
+                    ';Expires=' . date('D, d M Y H:i:s', $expires_at_timestamp);
+                header('Set-Cookie: ' . $sessionCookie, false);
+                header('Location: ' . 'http://' . $_GET['origin']);
+                die();
+            }
+        }
+        else {
+            die('There has been tampered with the SSO cookie!');
+        }
+    }
+    else if(empty($_GET['origin'])) {
+        die('ERROR: There was no origin to redirect back to');
     }
 ?>
 <html>
